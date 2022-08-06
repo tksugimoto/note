76e3e8d12b299a60678c2ed524ae36a9d2bce128
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2022-08-06 18:14:48 +0900

    feat: Android でページ共有を便利にする PWA

---
+++ share-page/README.md
@@ -0,0 +1,8 @@
+# Android の共有を便利にするツール(Progressive web apps, PWA)
+https://tksugimoto.github.io/note/share-page/
+
+## アイコン
+[アイコン素材ダウンロードサイト「icooon-mono」](https://icooon-mono.com/)
+の
+[シェアのアイコン素材](https://icooon-mono.com/00054-%E3%82%B7%E3%82%A7%E3%82%A2%E3%81%AE%E3%82%A2%E3%82%A4%E3%82%B3%E3%83%B3%E7%B4%A0%E6%9D%90/)
+を使わせていただきました。
---
+++ share-page/service-worker.js
@@ -0,0 +1,52 @@
+const CACHE_VERSION = '2022/08/11-17:20';
+const CACHE_NAME_SEPARATOR = ' '; // path 中の 半角スペース は url encode されるため混同される可能性がない
+const CACHE_NAME = `${self.registration.scope}${CACHE_NAME_SEPARATOR}${CACHE_VERSION}`;
+
+const urlsToCache = [
+	'./',
+	'./index.html',
+	'./share_target.html',
+];
+
+self.addEventListener('install', event => {
+	const promise = Promise.all([
+		caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)),
+		self.skipWaiting(),
+	]);
+	event.waitUntil(promise);
+});
+
+self.addEventListener('activate', event => {
+	const cacheDeletionPromise = caches.keys().then(cacheNames => {
+		const cachesToDelete = cacheNames.filter(cacheName => {
+			const [scope, version] = cacheName.split(CACHE_NAME_SEPARATOR);
+			return scope === self.registration.scope && version !== CACHE_VERSION;
+		});
+		const promises = cachesToDelete.map(cacheName => caches.delete(cacheName));
+		return Promise.all(promises);
+	});
+	event.waitUntil(Promise.all([
+		cacheDeletionPromise,
+		self.clients.claim(),
+	]));
+});
+
+self.addEventListener('fetch', event => {
+	const responsePromise = caches.open(CACHE_NAME).then(cache => {
+		return cache.match(event.request, {
+			ignoreSearch: true,
+		}).then(cacheResponse => {
+			if (cacheResponse) {
+				return cacheResponse;
+			}
+
+			return fetch(event.request).then(response => {
+				if (response.ok) {
+					cache.put(event.request, response.clone());
+				}
+				return response;
+			});
+		});
+	});
+	event.respondWith(responsePromise);
+});
