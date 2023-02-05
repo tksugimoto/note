23fb3a58c75c0f6c492a28ceb1a00b94a0028116
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2023-02-05 16:46:33 +0900

    feat: share-page: service-worker.js cache version up

--- share-page/service-worker.js
+++ share-page/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '2022/10/06-14:10';
+const CACHE_VERSION = '2023/02/05-16:45';
 const CACHE_NAME_SEPARATOR = ' '; // path 中の 半角スペース は url encode されるため混同される可能性がない
 const CACHE_NAME = `${self.registration.scope}${CACHE_NAME_SEPARATOR}${CACHE_VERSION}`;
 
